b67119c3c3969843d3232bddac209fcc9c58bac9
1512215 <huyhungpq64@gmail.com>
2018-11-13 02:02:54 +0700

    Update

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -3,52 +3,55 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { compose } from 'redux'
-import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import { firebaseConnect, isLoaded } from 'react-redux-firebase'
 
 
 class LogIn extends Component {
-    // componentDidMount() {
-    //     console.log("Line: 11");
-    //     console.log(this.props);
-    //     this.props.firebase.auth().onAuthStateChanged(user => {
-    //         console.log("aaaaa");
-    //         if (user) {
-    //             this.props.history.replace('/homepage');
-    //             console.log("ccccc");
-    //         }
-    //     });
-    // }
+    
+    static propTypes = {
+        router: PropTypes.object,
+        auth: PropTypes.object,
+        firebase: PropTypes.shape({
+            login: PropTypes.func.isRequired,    
+            logout: PropTypes.func.isRequired,
+        }),
+    }
+
+    componentDidMount() {
+
+        this.props.firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.history.push('/homepage');
+            }
+        });
+    }
 
     onLogin(){
         this.props.firebase.login({provider: 'google', 
                                    type: 'popup'})
                             .then(()=>{})
                             .catch((err) =>{});
-        this.props.firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                this.props.history.push('/homepage');
-            }
-        });        
+        
     }
 
     render() {
+        console.log("A");
         console.log(this.props.auth);
         if (!isLoaded(this.props.auth)) {
             return null;
         }
-            return(
-                <div className="row social-signin-container">
-                    <div className="col s10 offset-s1 text-align">
-                        <h4 id="sign-in-header">Sign In to start</h4>
-                        <a 
-                        href="#" className="icon-signin" 
-                        onClick={()=>this.onLogin()}>
-                        <i className="fa fa-google social-signin-icon" />
-                        Sign In With Google
-                        </a>
-                    </div>
+        return(
+            <div className="row social-signin-container">
+                <div className="col s10 offset-s1 text-align">
+                    <a 
+                    href="#" className="icon-signin" 
+                    onClick={()=>this.onLogin()}>
+                    <i className="fa fa-google social-signin-icon" />
+                    Sign In With Google
+                    </a>
                 </div>
-            )                
+            </div>
+        )                
     }
 }
 
@@ -56,7 +59,9 @@ class LogIn extends Component {
 
 const mapStateToProps = state => {
     return { 
-        auth: state.firebase.auth 
+        auth: state.firebase.auth
+        //auth is the props of this component
+        //state.firebase.auth: get state of store Reducer
     }
 }
 
